6b52a1277c6e44ddccf6d78c36b8d6a669c02cb7
knlghtfreakg <roshanahamedmba2022@gmail.com>
2025-02-27 20:40:13 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,335 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dining",
+      "name": "Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4790921253771288,
+          "pitch": 0.025462309663820903,
+          "rotation": 0,
+          "target": "1-foyer"
+        },
+        {
+          "yaw": 0.287398423151517,
+          "pitch": 0.03592253475231999,
+          "rotation": 0,
+          "target": "5-pantry"
+        },
+        {
+          "yaw": -0.2935292963574305,
+          "pitch": 0.03906305407747723,
+          "rotation": 0,
+          "target": "4-mud-room"
+        },
+        {
+          "yaw": 0.00980609267903887,
+          "pitch": 0.11439637486805232,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -3.0955393287876714,
+          "pitch": 0.41548961441921506,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-foyer",
+      "name": "Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.576730952530097,
+          "pitch": 0.2211612047808238,
+          "rotation": 0,
+          "target": "0-dining"
+        },
+        {
+          "yaw": 2.0728590896433916,
+          "pitch": 0.20406575462425636,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9003140189730825,
+          "pitch": 0.15802777603325424,
+          "rotation": 0,
+          "target": "5-pantry"
+        },
+        {
+          "yaw": -0.9220986477261555,
+          "pitch": 0.1580315286238516,
+          "rotation": 0,
+          "target": "4-mud-room"
+        },
+        {
+          "yaw": 3.1262356861909595,
+          "pitch": 0.22127829584038317,
+          "rotation": 0,
+          "target": "0-dining"
+        },
+        {
+          "yaw": 3.1198662656900904,
+          "pitch": 0.09136903397561547,
+          "rotation": 0,
+          "target": "3-living-room"
+        },
+        {
+          "yaw": -2.5830351396935,
+          "pitch": 0.15009666218284323,
+          "rotation": 0,
+          "target": "1-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -6.270638408523155e-8,
+          "pitch": 0.18433873824131553,
+          "rotation": 0,
+          "target": "0-dining"
+        },
+        {
+          "yaw": 0.20252897382978752,
+          "pitch": 0.0324441663283892,
+          "rotation": 0,
+          "target": "5-pantry"
+        },
+        {
+          "yaw": -0.20044410128269163,
+          "pitch": 0.03682855612196434,
+          "rotation": 0,
+          "target": "4-mud-room"
+        },
+        {
+          "yaw": -0.0022323260378556142,
+          "pitch": 0.035350125243512664,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -0.8733190586691855,
+          "pitch": 0.04754785960330565,
+          "rotation": 0,
+          "target": "1-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mud-room",
+      "name": "Mud room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6336033182102643,
+          "pitch": 0.1844939561276071,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": 2.8237477529344623,
+          "pitch": 0.12411979841069964,
+          "rotation": 0,
+          "target": "0-dining"
+        },
+        {
+          "yaw": 2.992819248222448,
+          "pitch": 0.09873183757821025,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pantry",
+      "name": "Pantry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.899025089833536,
+          "pitch": 0.16899483349045497,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -3.077379535916375,
+          "pitch": 0.08103508346936295,
+          "rotation": 0,
+          "target": "3-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
